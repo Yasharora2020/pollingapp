e474426f1c7a298f72a8181070b7bb1c215dcb4d
Yash <yashvardhanarora@gmail.com>
2023-04-07 09:27:08 +1000

    added config.py

---
+++ confi.py
@@ -0,0 +1,10 @@
+REGION = 'region name'
+USER_POOL_ID = 'secret'
+CLIENT_ID = 'secret'
+CLIENT_SECRET = 'secret'
+SECRET_KEY = 'your_secret_key_here'
+SQLALCHEMY_DATABASE_URI = 'mysql://admin:password@pollingdb.sample.region.rds.amazonaws.com:3306/pollingdb'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///polls.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DOMAIN = 'https://app.auth.ap-southeast-2.amazoncognito.com'
+COOKIE_AGE_SECONDS = 1800
